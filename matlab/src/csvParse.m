0eed9ecea1d9f588dd006bc0f65389e9c885e4f4
Kevin Sprong <ksprong@mitre.org>
2014-05-05 18:43:37 -0400

    .

--- matlab/src/csvParse.m
+++ matlab/src/csvParse.m
@@ -4,8 +4,11 @@ fid = fopen(filePathIn);
 line = 'z';
 while ischar(line)
     line = fgetl(fid);
-    parsedLine = textscan(line, '%s', 'Delimiter', ','); % unknown schema
-    parsedLine = parsedLine{1}; % cell -> array
+    if ~ischar(line)
+        break; 
+    end    
+    parsedLine = textscan(line, '%s', 'Delimiter', ','); % unknown schema    
+    parsedLine = parsedLine{1}; % cell -> array     
     fifthNumber = str2double(parsedLine{5}); % index into array
 end
 fclose(fid);
